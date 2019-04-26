a470e32e7718907d09c23e6d51276a7ff57a5388
Justin <jdlow2@gmail.com>
2019-04-26 09:37:20 -0400

    testing opendir and readdir

--- fork.c
+++ fork.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +10,7 @@ int main(int argc, char *argv[])
     printf("argc is %d\n", argc);
   }
   printf("hello, world! (pid: %d)\n", (int) getpid());
+  int x = 42;
   int rc = fork();
 
   if (rc < 0) {
@@ -15,8 +18,18 @@ int main(int argc, char *argv[])
     exit(1);
   } else if (rc == 0) {
     printf("child process here. (pid: %d)\n", (int) getpid());
+    printf("x is %d and is at %p\n", x, &x);
+    x = 41;
+    printf("x is now %d at the address %p\n", x, &x);
   } else {
     printf("hello, parent here. (pid: %d) of child %d\n", (int) getpid(), rc );
+    printf("x is %d and is at %p\n", x, &x);
+    x = 23;
+    printf("x is now %d at the address %p\n", x, &x);
+    DIR *dir = opendir(".");
+    struct dirent *file;
+    while ((file = readdir(dir)) != NULL)
+      printf("%s\n", file->d_name);
   }
 
   return 0;
