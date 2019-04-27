f9bc91341e778d3ac9b99c865bea2a941cece073
dividedsky <jdlow2@gmail.com>
2019-04-27 11:58:31 -0400

    adds array string to print_string

--- print_string.c
+++ print_string.c
@@ -3,11 +3,16 @@
 void print_string(char *s);
 int main(void) {
   char *my_string = "hello! hi!\n";
+  char array_string[] = "this is an array\n";
   print_string(my_string);
+  print_string(array_string);
 
 }
 
 /* takes a character pointer and prints a string */
+/* this works fine with an array string, as well,
+   because when the array string is passed, it's 
+   simply passed as a pointer */
 void print_string(char * s)
 {
   for (int i = 0; *(s + i) != '\0'; i++) 
